d882f0ec6034d5bfdc82f80351efea6d046f53d7
Briana Eng <brianaeng@gmail.com>
2016-10-23 16:26:09 -0700

    Added functionality to sign up on the Task List site (and sign in).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to index_path
+    else
+      redirect_to new_user_path
+    end
   end
 
   def edit
@@ -24,6 +32,11 @@ class UsersController < ApplicationController
   private
   def find_user
     @user = User.find(params[:id].to_i)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
